553e0de7f1196d16c31941b5cae8f4eadd1b83d9
Bell <bunyaphon536@gmail.com>
2026-01-08 03:30:16 +0700

    upload page 6 on lecture1

---
+++ lecture1/lec1_6.java
@@ -0,0 +1,8 @@
+public class lec1_6 {
+    public static void main(String[] args) {
+        System.out.print("line 1");
+        System.out.print("line 2");
+        System.out.println("line 3");
+        System.out.println("line 4");
+    }
+}
