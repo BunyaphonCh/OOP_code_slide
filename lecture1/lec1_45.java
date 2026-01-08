e686aaf4d1be11697ced08b7c7bbd8018283d6ea
Bell <bunyaphon536@gmail.com>
2026-01-08 11:21:50 +0700

    upload code lec1

---
+++ lecture1/lec1_45.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class lec1_45 {
+    public static void main(String args[]) {
+        int x = 5, y;
+        boolean b1;
+        float z = 3.414f;
+        double w;
+        y = 4;
+        b1 = (x > y); // b1 = true
+        w = x * 3.2; // w = 16.0
+        
+        System.out.println("x=" + x + "y=" + y);
+        System.out.println("b1=" + b1);
+        System.out.println("z=" + z + "w=" + w);
+    }
+}
---
+++ lecture1/lec1_7.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class lec1_7 {
+    public static void main(String[] args) {
+        System.out.println("line 1");
+        System.out.print("line 2");
+        System.out.println("line 3");
+        System.out.print("line 4");
+    }
+}
+
+/*
+line 1
+line 2line 3
+line 4
+*/
